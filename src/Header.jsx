43061b8d239ef6a039e2228b286ab77214545a3b
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-24 15:11:50 +0530

    completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,29 +4,21 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
-    <div className="text-center mt-4 text-4xl text-blue-700 font-extrabold">Markdown</div>
-    <div className="header flex justify-between items-center md:mx-[450px] mx-6 mt-10">
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          `${
-            isActive ? "text-blue-700" : "text-white"
-          } font-bold text-sm md:text-2xl hover:text-blue-700 cursor-pointer`
-        }
-      >
-        Custom Manipulation
-      </NavLink>
-      <NavLink
-        to="/library"
-        className={({ isActive }) =>
-          `${
-            isActive ? "text-blue-700" : "text-white"
-          } font-bold text-sm md:text-2xl hover:text-blue-700 text-white cursor-pointer`
-        }
-      >
-        Using React Library
-      </NavLink>
-    </div>
+      <div className="text-center mt-4 text-4xl text-blue-700 font-extrabold">Markdown</div>
+      <div className="header flex justify-between items-center md:mx-[450px] mx-6 mt-10">
+        <NavLink
+          to="/"
+          className="font-bold text-sm md:text-2xl hover:text-blue-700 cursor-pointer text-white"
+        >
+          Custom Manipulation
+        </NavLink>
+        <NavLink
+          to="/library"
+          className="font-bold text-sm md:text-2xl hover:text-blue-700 text-white cursor-pointer"
+        >
+          Using React Library
+        </NavLink>
+      </div>
     </>
   );
 };
