c322fd4f5000b6cd36bd2356ee83c3815c6e3338
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-24 12:35:20 +0530

    custom markdown done

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,23 @@ const App = () => {
   };
 
   const handleFormat = (format) => {
-    const selectedText = document.getElementById('text').value;
+    const textarea = document.getElementById('text');
+    const selectionStart = textarea.selectionStart;
+    const selectionEnd = textarea.selectionEnd;
+    const selectedText = textarea.value.substring(selectionStart, selectionEnd);
     let newText;
 
     switch (format) {
       case 'bold':
-        newText = toggleMarkdownSyntax(selectedText, '**', formatBold);
+        newText = toggleMarkdownSyntax(textarea.value, '**', selectionStart, selectionEnd, formatBold);
         setFormatBold(!formatBold);
         break;
       case 'italic':
-        newText = toggleMarkdownSyntax(selectedText, '_', formatItalic);
+        newText = toggleMarkdownSyntax(textarea.value, '_', selectionStart, selectionEnd, formatItalic);
         setFormatItalic(!formatItalic);
         break;
       case 'heading':
-        newText = toggleHeading(selectedText);
+        newText = toggleHeading(textarea.value, selectionStart, selectionEnd);
         setFormatHeading(!formatHeading);
         break;
       case 'link':
@@ -38,22 +41,21 @@ const App = () => {
         break;
     }
 
-    setText(newText);
+    const updatedText = textarea.value.substring(0, selectionStart) + newText + textarea.value.substring(selectionEnd);
+    setText(updatedText);
   };
 
-  const toggleMarkdownSyntax = (selectedText, syntax, currentState) => {
-    const wrappedText = syntax + selectedText + syntax;
-    return currentState ? selectedText.slice(syntax.length, -syntax.length) : wrappedText;
+  const toggleMarkdownSyntax = (text, syntax, start, end, currentState) => {
+    const wrappedText = syntax + text.substring(start, end) + syntax;
+    return currentState ? text.substring(start + syntax.length, end - syntax.length) : wrappedText;
   };
 
-  const toggleHeading = (selectedText) => {
-    const isImage = selectedText.startsWith('![[') && selectedText.endsWith(']]');
+  const toggleHeading = (text, start, end) => {
     return formatHeading
-      ? selectedText
-      : isImage
-      ? '## ' + selectedText.substring(3, selectedText.length - 2) + ' ##'
-      : '# ' + selectedText;
+      ? text.substring(start, end)
+      : '# ' + text.substring(start, end);
   };
+  
 
   const wrapInTag = (selectedText, tag, attribute) => {
     return `<${tag} ${attribute}="${selectedText}" target="_blank">${selectedText}</${tag}>`;
@@ -67,33 +69,31 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center md:min-h-screen mt-10 px-4">
-  <div className='max-w-5xl'>
-    <div className='font-extrabold text-2xl md:text-4xl text-blue-600 mb-6 md:mb-10 text-center'>Markdown using own custom string manipulation</div>
-    <div className="flex flex-col items-center">
-      <div className="flex flex-wrap justify-center mb-4 md:gap-4 gap-1">
-        <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('bold')}>{formatBold ? 'Unbold' : 'Bold'}</button>
-        <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('italic')}>{formatItalic ? 'Unitalic' : 'Italic'}</button>
-        <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('heading')}>{formatHeading ? 'S' : 'H'}</button>
-        <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('link')}>Link</button>
-      </div>
-      <textarea
-        id="text"
-        value={text}
-        onChange={handleTextChange}
-        className="text-white w-full md:w-[400px] h-20 md:h-40 mb-4 md:mb-7 px-2"
-      />
-      <div>
-        <strong className='font-bold text-lg md:text-2xl flex justify-center items-center mb-2'>Preview:</strong>
-        <div style={{ fontSize: formatHeading ? '24px' : '16px' }} className='w-full md:w-[400px] md:break-all'>
-          <div dangerouslySetInnerHTML={{ __html: parseMarkdown(text) }} />
+    <div className="flex flex-col md:min-h-screen mt-24 px-4">
+      <div className='max-w-5xl md:max-w-full'>
+        <div className='font-extrabold text-2xl md:text-4xl text-blue-600 mb-6 md:mb-10 text-center'>Markdown using own custom string manipulation</div>
+        <div className="flex flex-col items-center">
+          <div className="flex flex-wrap justify-center mb-4 md:gap-4 gap-1">
+            <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('bold')}>{formatBold ? 'Unbold' : 'Bold'}</button>
+            <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('italic')}>{formatItalic ? 'Unitalic' : 'Italic'}</button>
+            <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('heading')}>{formatHeading ? 'S' : 'H'}</button>
+            <button className='w-full md:w-28 text-blue-300 mb-2 md:mb-0' onClick={() => handleFormat('link')}>Link</button>
+          </div>
+          <textarea
+            id="text"
+            value={text}
+            onChange={handleTextChange}
+            className="text-white w-full md:w-[400px] h-20 md:h-40 mb-4 md:mb-7 px-2"
+          />
+          <div>
+            <strong className='font-bold text-lg md:text-2xl flex justify-center items-center mb-2'>Preview:</strong>
+            <div style={{ fontSize: formatHeading ? '36px' : '16px' }} className='w-full md:w-[400px] md:break-all'>
+              <div dangerouslySetInnerHTML={{ __html: parseMarkdown(text) }} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-  
   );
 };
 
