304e54bcfbd26392f21ade8ee9aa4adaba2d217e
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-24 15:31:09 +0530

    header swap

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,17 +6,19 @@ const Header = () => {
     <>
       <div className="text-center mt-4 text-4xl text-blue-700 font-extrabold">Markdown</div>
       <div className="header flex justify-between items-center md:mx-[450px] mx-6 mt-10">
-        <NavLink
-          to="/"
-          className="font-bold text-sm md:text-2xl hover:text-blue-700 cursor-pointer text-white"
-        >
-          Custom Manipulation
-        </NavLink>
+        
         <NavLink
           to="/library"
           className="font-bold text-sm md:text-2xl hover:text-blue-700 text-white cursor-pointer"
         >
           Using React Library
+        </NavLink>
+
+        <NavLink
+          to="/"
+          className="font-bold text-sm md:text-2xl hover:text-blue-700 cursor-pointer text-white"
+        >
+          Custom Manipulation
         </NavLink>
       </div>
     </>
